3dd2507f68340c0b13c5c69be11c978d5608e21f
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-06-16 10:18:44 +0530

    Time: 23 ms (57.41%), Space: 11.6 MB (67.29%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     
     
+
     
     vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
         vector<int> v1;
@@ -30,17 +31,35 @@ public:
           }
             v2.push_back(count);
         }
-        vector<int> ans;
+        vector<int> anss;
         sort(v2.begin(),v2.end());
         for(int i=0; i<v1.size(); i++){
-            int it=upper_bound(v2.begin(),v2.end(),v1[i])-v2.begin();
-       int t=v2.size()-it;
-            // t+=1;
-             ans.push_back(t); 
+ int target=v1[i];
+
+      int low=0;
+            int n=v2.size();
+      int high =n-1;
+int ans=INT_MAX;
+while(low<=high){
+    int mid=low+(high-low)/2;
+    if(v2[mid]>target){
+        ans=mid;
+        high=mid-1;
+    }else{
+low=mid+1;
+    }
+}
+if(ans==INT_MAX){
+    int tt=0;
+    anss.push_back(tt);
+}else{
+       int t=v2.size()-ans;
+        
+             anss.push_back(t); 
             
             }
             // ans.push_back(count);
-        
-        return ans;
+        }
+        return anss;
     }
 };
