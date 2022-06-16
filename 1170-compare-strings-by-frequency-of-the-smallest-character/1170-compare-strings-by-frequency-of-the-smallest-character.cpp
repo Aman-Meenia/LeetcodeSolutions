6d59634da209fb7330f909b07e942674bd133bfb
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-06-16 08:54:23 +0530

    Time: 25 ms (55.53%), Space: 11.6 MB (85.65%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    
+    
     vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
         vector<int> v1;
         vector<int> v2;
@@ -28,16 +31,16 @@ public:
             v2.push_back(count);
         }
         vector<int> ans;
-        
+        sort(v2.begin(),v2.end());
         for(int i=0; i<v1.size(); i++){
-            int count=0;
-            for(int j=0; j<v2.size(); j++){
-                if(v2[j]>v1[i]){
-                    count++;
-                }
+            int it=upper_bound(v2.begin(),v2.end(),v1[i])-v2.begin();
+       int t=v2.size()-it;
+            // t+=1;
+             ans.push_back(t); 
+            
             }
-            ans.push_back(count);
-        }
+            // ans.push_back(count);
+        
         return ans;
     }
 };
