1cf7b5ca5b798173b1d833968889b75e75412d91
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-06-17 19:56:51 +0530

    Time: 1256 ms (44.97%), Space: 202.9 MB (44.61%) - LeetHub

--- 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.cpp
+++ 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
-    
-    
-//     stattic int cmp(vector<int> &a , vector<int> &b){
-        
-//     }
-    
+//       USING BFS 
+
     vector<vector<int>> highestRankedKItems(vector<vector<int>>& grid, vector<int>& pricing, vector<int>& start, int k) {
         int n=grid.size();
         int m=grid[0].size();
+//          MAKE DP TO CHECK THAT WE DO NOT REVISIT THE SAME INDEX   
+//          IF WE VISIT THAT  INDEX WE MAKE IT TRUE 
         vector<vector<int> > dp(n,vector<int>(m,0));
+        
+
      int array1[4]={0,0,1,-1};
      int array2[4]={1,-1,0,0};
         
+//         IN THIS ARRAY WE STORE
+//      ({DISTANCE , PRICE , ROW INDEX , COLUM INDEX })
         vector<vector<int> > v;
         int distance=0;
 queue<pair<int,int> > q;
+//         PUSH STARTING INDEX IN QUEUE 
         q.push(make_pair(start[0],start[1]));
         int xx=start[0];
         int yy =start[1];
+//         CHECKING IF WE PUSH STARING INDEX IN OUR VECTOR 
                if(xx>=0 && yy>=0 && xx<n && yy<m &&  dp[xx][yy]==0 &&grid[xx][yy]>=pricing[0] &&grid[xx][yy]<=pricing[1]){
-                   v.push_back({0,grid[xx][yy],xx,yy});
+                   
+     v.push_back({0,grid[xx][yy],xx,yy});
+                   
                }
+//         MAKING OUR STARTING INDEX TRUE IN OUT DP ARRAY 
         dp[start[0]][start[1]]=1;
         
+        
+//          SIMPLE BFS AND CHECKING THE SAME CONDITION THAT WE CHECK FOR OUR STARTING INDEX 
         while(!q.empty()){
             distance+=1;
-            // if(v.size()>=k){
-            //     break;
-            // }
+            
+            
             
             int size=q.size();
             for(int i=0; i<size; i++){
@@ -39,19 +47,23 @@ queue<pair<int,int> > q;
                 for(int j=0; j<4; j++){
                     int x=a+array1[j];
                     int y=b+array2[j];
-                    // cout<<x<<" y "<<y<<endl;
-                    // if(x==2 && y==1 || x==2 && y==0){
-                    //  cout<<" grid "<<grid[x][y]<<endl;
-                    //    cout<<"dp "<< dp[x][y]<<endl;
-                    // }
+//                   CONDTION CHECK 
                     if(x>=0 && y>=0 && x<n && y<m &&  dp[x][y]==0 &&grid[x][y]>=pricing[0] &&grid[x][y]<=pricing[1]){
-                        // cout<<"aman "<<endl;
+          
                         q.push(make_pair(x,y));
                         dp[x][y]=1;
               v.push_back({distance,grid[x][y],x,y});
+                        
+                    
+                        
+//       IF OUR CONDITION IS FAIL  
+//          IF NOT VISITED  AND GRID[X][Y] IS NOT EQUAL TO ZERO THAM WE MOVE TO THAT INDEX THAT'S WHY WE PUSH IT IN QUEUE BUT NOT IN OUR VECTOR v
+                        
                     }else if(x>=0 && y>=0 && x<n && y<m && grid[x][y]!=0 && dp[x][y]==0 ){
+                        
                         dp[x][y]=1;
                           q.push(make_pair(x,y));
+                        
                     }
                     
                 }
@@ -68,7 +80,9 @@ queue<pair<int,int> > q;
         sort(v.begin(),v.end());
         
         int tt =v.size();
-        // cout<<"tt "<<tt<<endl;
+
+        
+        
         int p=min(k,tt);
         for(int i=0; i< p; i++){
             ans.push_back({v[i][2],v[i][3]});
