a56dcb1f093fdd295952e6d5198c471f512d9a57
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-06-20 09:52:31 +0530

    Time: 127 ms (46.04%), Space: 31.3 MB (51.98%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    static int cmp(string s1, string s2){
+        return s1.size()>s2.size();
+    }
+    int minimumLengthEncoding(vector<string>& words) {
+        sort(words.begin(), words.end(),cmp);
+        // reverse(words.begin(),words.end());
+        // for(auto a:words){
+        //     cout<<a<<" ";
+        // }cout<<endl;
+        unordered_set<string> st;
+        int ans=0;
+        int size=words.size();
+        for(int i=0; i<size; i++){
+            string t=words[i];
+            if(st.find(t)!=st.end()){
+                continue;
+            }
+            reverse(t.begin(),t.end());
+            string tt;
+            ans+=t.size();
+            ans+=1;
+            for(int k=0; k<t.size(); k++){
+                tt.push_back(t[k]);
+                string p=tt;
+                reverse(p.begin(),p.end());
+                st.insert(p);
+            }
+            
+        }
+        return ans;
+        
+    }
+};
