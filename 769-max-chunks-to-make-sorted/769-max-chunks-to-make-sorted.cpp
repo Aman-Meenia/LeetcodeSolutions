aae481595e50732373074fc5c6e3715a76b0065b
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-06-27 10:33:19 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (74.54%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& v) {
-      
-vector<int> temp(v.begin(),v.end());
-        sort(temp.begin(),temp.end());
-        unordered_map<int,int> mp;
-        int n=v.size();
+        int maximum=0;
         int count=0;
+        int n=v.size();
         for(int i=0; i<n; i++){
-            mp[temp[i]]+=1;
-            if(mp[temp[i]]==0){
-  auto it=mp.find(temp[i]);
-
-                mp.erase(it);
+          maximum=max(maximum,v[i]);
+            if(maximum==i){
+                count++;
             }
-            
-            if(mp.find(v[i])!=mp.end()){
-                mp[v[i]]-=1;
-                   if(mp[v[i]]==0){
-                auto it=mp.find(v[i]);
-                mp.erase(it);
-            }
-            }else{
-                mp[v[i]]=-1;
-            }
-            
-            if(mp.size()==0) count+=1;
-            
-
         }
-              return count;  
+        return count;
     }
 };
