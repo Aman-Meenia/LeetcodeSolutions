16f3ec991d8314b1c9f257c34db82353e23e4e38
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-01 22:06:04 +0530

    Time: 51 ms (22.26%), Space: 10.8 MB (61.42%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string s1, string s2) {
-int n=s1.size();
-        int m=s2.size();
-
-        int dp[n+1][m+1];
-memset(dp, 0 , sizeof dp);
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
-                }
-                
-                
+    int dp[1001][1001];
+int fun(string &s1, string &s2 , int n , int m ){
+    if(n==0 || m==0){
+        return 0;
+    }
+     if(dp[n][m]!=-1) return dp[n][m];
+    if(s1[n-1]==s2[m-1]){
+      return dp[n][m]=1+fun(s1,s2,n-1,m-1);
+        
+    }else{
+       return dp[n][m]=max(fun(s1,s2,n-1,m),fun(s1,s2,n,m-1));  
+    }
+    
+    
+    
 }
-        }
-        return dp[n][m];
+    
+    
+    
+    int longestCommonSubsequence(string s1, string s2) {
+        memset(dp, -1 ,sizeof dp);
+        int n=s1.size();
+        int m=s2.size();
+       return fun(s1,s2,n,m); 
     }
 };
