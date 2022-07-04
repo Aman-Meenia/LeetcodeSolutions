2e89ee7cdfc98cdd7fb117ebb28041bab6dac88f
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-04 11:00:24 +0530

    Time: 92 ms (82.08%), Space: 13.8 MB (79.01%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,64 +1,51 @@
 class Solution {
 public:
-    static int cmp(string &s1, string &s2){
-        int n=s1.size();
-        int m=s2.size();
-        
-        return s1.size()<s2.size();
+    
+    static int cmp(string & a , string & b ){
+        return a.size()<b.size();
     }
-    
-    
-    
+    bool compare(string & a, string & b){
+        int n=0;
+        int m=0;
+        int count=0;
+        while(n<a.size()&&m<b.size()){
+            if(a[n]==b[m]){
+                n++;
+                m++;
+            }else{
+                n++;
+                count+=1;
+                if(count>1) return false;
+            }
+        }
+        return true;
+    }
     int longestStrChain(vector<string>& s) {
-        sort(s.begin(),s.end(),cmp);
-        // for(auto a:s){
-        //     cout<<a<<" ";
-        // }cout<<endl;
+        int ans=1;
         int n=s.size();
+        sort(s.begin(),s.end(),cmp);
+     
+//       using LIS 
         vector<int> dp(n,1);
         for(int i=0; i<n; i++){
-   
-            
-string temp=s[i];
-for(int j=0; j<i; j++){
-string t=s[j];
-int count=0;
-int check=0;
-if(temp.size()==(t.size()+1)){
-int ttt=0;
-// cout<<t<<" temp "<<temp<<endl;
-for(int p=0; p<t.size(); p++){
-if(t[p] != temp[ttt]   &&   check == 0){
-ttt++;
-count++;
-check=1;
+            string temp=s[i];
+            for(int j=0; j<i; j++){
+
+if(s[j].size()+1==temp.size()&&compare(temp,s[j])){
+    if(dp[j]+1>dp[i]){
+        dp[i]=dp[j]+1;
+    }
 }
-if(t[p] != temp[ttt]){
-    count++;
-}
-
-ttt++;
-}
-
-if(count<=1){
-dp[i]=max(dp[i],1+dp[j]);
-}
-}
-
-
-
-
-
-}
-            
+                
+                
+            }
+            ans=max(ans,dp[i]);
         }
-         int ans=0;
-        for(auto a:dp){
+        
+        
+        
+        
 
-           ans=max(ans,a);
-        }
-
-       
         return ans;
         
     }
