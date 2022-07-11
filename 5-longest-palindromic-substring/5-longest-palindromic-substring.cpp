83c908afad0affd127fdc9281cc9bd67e8e05376
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-11 12:07:23 +0530

    Time: 373 ms (37.30%), Space: 11.5 MB (55.23%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,53 +1,46 @@
-class Solution {
+class Solution 
+{
 public:
-    string longestPalindrome(string s) {
-int size=s.size();
+    string longestPalindrome(string s)
+    {
+         int n=s.size();
+
+int dp[n][n];
+memset(dp,0,sizeof dp);
 int start=0;
-int end=0;
-int length=1;
-// EVEN LENGTH
-
-for(int i=0; i<size; i++) {
-
-	int l=i;
-	int r=i+1;
-	while(l>=0 && r<size){
-		if(s[l]==s[r]){
-			l--;
-			r++;
-		}else{
-			break;
-		}
-	}
-	int len=r-l-1;
-	if(len>length){
-		length=len;
-		start=l+1;
-		end=r-1;
-	}
+int maxlength=0;
+for(int i=0; i<n; i++){
+    dp[i][i]=1;
+    start=i;
+    maxlength=1;
 }
-
-// ODD LENGTH
-	for(int i=0; i<size; i++) {
-
-	int l=i;
-	int r=i;
-	while(l>=0 && r<size){
-		if(s[l]==s[r]){
-			l--;
-			r++;
-		}else{
-			break;
-		}
-	}
-	int len=r-l-1;
-	if(len>length){
-		length=len;
-		start=l+1;
-		end=r-1;
-	}
+    
+for(int i=0; i<n-1; i++){
+    if(s[i]==s[i+1])
+            {
+                dp[i][i+1]=1;
+                start=i;
+                maxlength=2;
+            }
 }
-string ans =s.substr(start,length);
-        return ans;
+for(int j=2; j<n; j++){
+int t=j;
+    for(int i=0; i<n-j; i++){
+                if(s[i]==s[t] && dp[i+1][t-1]==1)
+                {
+                    dp[i][t]=true;
+
+
+                    if(t-i+1>maxlength){
+                        start=i;
+                        maxlength=t-i+1;
+                    }
+                 
+                }
+t++;
+
+    }
+}
+        return s.substr(start,maxlength);
     }
 };
