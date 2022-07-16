033d254610d8a83ec0b32ea656d5e9b8c4994019
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-16 14:08:02 +0530

    Time: 15 ms (87.05%), Space: 13.1 MB (95.83%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,36 +1,55 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& v) {
+        int rowcheck=0;
+        int colcheck=0;
         int n=v.size();
         int m=v[0].size();
-        vector<pair<int,int> > temp;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i=0; i<m; i++){
+            if(v[0][i]==0){
+                colcheck=1;
+            }
+        }
+          for(int i=0; i<n; i++){
+            if(v[i][0]==0){
+                rowcheck=1;
+            }
+        }
+        
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
                 if(v[i][j]==0){
-                temp.push_back({i,j});
+                    v[i][0]=0;
+                    v[0][j]=0;
                 }
             }
         }
-        int size=temp.size();
-        for(int i=0; i<size; i++){
-            int x=temp[i].first;
-            int y=temp[i].second;
-            for(int k=0; k<m; k++){
-                v[x][k]=0;
+        
+   for(int i=n-1; i>=1; i--){
+            for(int j=m-1; j>=1; j--){
+                if(v[i][0]==0 || v[0][j]==0){
+                    v[i][j]=0;
+                }
             }
-             for(int k=0; k<n; k++){
-                   v[k][y]=0; 
-            }
-            
-            
-            
+        }
+        
+        
+        if(colcheck==1){
+            for(int i=0; i<m; i++){
+           v[0][i]=0;
+ 
+           }
+        }
+        
+        if(rowcheck==1){
+              for(int i=0; i<n; i++){
+      v[i][0]=0;
+
+        }
         }
         
         
         
         
-        
-        
-
     }
 };
