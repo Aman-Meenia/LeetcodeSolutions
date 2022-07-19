97501f3c47e4da61ac75bfa41f4a7f84b27c16f9
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-19 18:36:32 +0530

    Time: 10 ms (26.38%), Space: 9.6 MB (5.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,20 +4,21 @@ public:
         int n = v.size();
         int m = v[0].size();
         int low = 0;
-        int high = m-1;
-        
-        while(low<n && high>=0){
-
-            if( v[low][high] == target ) return true;
-            
-            if( v[low][high] > target ){
-              high--;  
+        int high = m*n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(v[mid/m][mid%m]==target) return true;
+            if(v[mid/m][mid%m]>target){
+                high=mid-1;
             }else{
-                low++;
+                low=mid+1;
             }
-            
         }
         return false;
+        
+        
+        
+ 
         
     }
 };
