333f700d02bee14ea4f1f774aab61b3872eb1d40
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-21 00:33:31 +0530

    Time: 374 ms (86.19%), Space: 8.3 MB (94.51%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     
-    int dp[301][301];
-    int maxcost(vector<int> & v, int i , int j){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans=INT_MIN;
-        for(int k=i; k<=j; k++){
-            int cost=maxcost(v,i,k-1) + maxcost(v,k+1,j) + v[k] * v[i-1] * v[j+1];
-            ans=max(ans,cost);
-        }
-        return dp[i][j]=ans;
-    }
-    
-    
-    
     int maxCoins(vector<int>& v) {
+        int size=v.size();
         v.push_back(1);
         v.insert(v.begin(),1);
-        memset(dp,-1,sizeof dp);
-        return maxcost(v,1,v.size()-2);
+        int dp[v.size()][v.size()];
+        memset(dp,0,sizeof dp);
+        
+        for(int i=size; i>=1; i--){
+            
+            for(int j=1; j<=size; j++){
+                if(i>j) continue;
+                 int ans=INT_MIN;
+        for(int k=i; k<=j; k++){
+            int cost=dp[i][k-1] + dp[k+1][j] + v[k] * v[i-1] * v[j+1];
+            ans=max(ans,cost);
+        }
+    dp[i][j]=ans;
+            }
+            
+        }
+        
+        
+        
+        
+        return dp[1][size];
     }
 };
