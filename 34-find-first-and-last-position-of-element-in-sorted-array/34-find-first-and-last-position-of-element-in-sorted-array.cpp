f0521b5a1e1b5e5bcfd2629967a17a8227454cef
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-25 06:52:10 +0530

    Time: 12 ms (60.23%), Space: 13.5 MB (83.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& v, int target) {
-        int a =INT_MAX, b; 
-        int n=v.size();
-        int cnd=1;
-        for(int i=0; i<n; i++){
-            if(v[i]==target&& cnd==1 ){
-                cnd=0;
-                a=i;
-                b=i;
-            }else if(cnd==0 && v[i]==target){
-                b=i;
-            }else if(cnd==0){
-                return {a,b};
+        int n = v.size();
+        int a = INT_MAX;
+        int b =INT_MAX;
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(v[mid]==target){
+                a=mid;
+                high=mid-1;
+            }
+            if(v[mid]<target){
+                low=mid+1;
+            }else if(v[mid]>target){
+                high=mid-1;
             }
         }
-        if(a!=INT_MAX) return {a,b};
-        return {-1,-1};
+            low = 0;
+            high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(v[mid]==target){
+                b=mid;
+                low=mid+1;
+            }
+            if(v[mid]<target){
+                low=mid+1;
+            }else if(v[mid]>target){
+                high=mid-1;
+            }
+        }
+        
+        
+if(a==INT_MAX) return {-1,-1};
+        return {a,b};
+        
+        
     }
 };
