06324b30899c772017a3b3b61ac7c4f6a16cbe32
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-25 07:02:29 +0530

    Time: 114 ms (26.35%), Space: 25.8 MB (18.06%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -6,15 +6,17 @@ public:
         vector<int> dp(n,INT_MAX);
         dp[0]=0;
         int tt=0;
-        for(int i=0; i<n; i++){
+//           we use tt to store the maximum index upto which we are already visited so next time we start from maximum element
+ for(int i=0; i<n; i++){
 if(dp[i]==INT_MAX) continue;
-            for(int j=max(i+minJump,tt); j<=min(i+maxJump,n-1); j++){
-if(s[j]=='1') continue;
+for(int j=max(i+minJump,tt); j<=min(i+maxJump,n-1); j++){
+             if(s[j]=='1') continue;
                 dp[j]=1;
+    
                 if(j==n-1){
-                    cout<<" j is "<<j<<endl;
                return dp[j];
                 }
+    
             }
             tt=i+maxJump;
         }
