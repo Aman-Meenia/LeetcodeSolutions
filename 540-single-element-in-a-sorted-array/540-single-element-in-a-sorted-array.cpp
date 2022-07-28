2d32741cbdf2c06b44a91d204bafca3d844eb302
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-28 06:05:47 +0530

    Time: 40 ms (54.57%), Space: 22.3 MB (55.04%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,32 +1,10 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& v) {
-        
-        if(v.size()==1) return true;
-        int low = 0;
-        int high = v.size()-1;
-        while(low<=high){
-           
-            int mid = low+(high-low)/2;
-            cout<<mid<<endl;
-            if(mid==0){
-                if(v[mid]!=v[mid+1]) return v[mid];
-                else low=mid+1;
-            }else if(mid==v.size()-1){
-                  if(v[mid]!=v[mid-1]) return v[mid];
-                else high=mid-1;
-            }else{
-               if(v[mid]!=v[mid-1] && v[mid]!=v[mid+1]){
-                   return v[mid];
-               }else if(mid%2==0 && v[mid-1]==v[mid]||mid%2!=0 &&v[mid]==v[mid+1]){
-               high=mid-1; 
-            }else{
-                low=mid+1;
-                   // cout<<"check "<<low<<endl;
-            }
-            }
-            
+        int t=0;
+        for(auto a:v){
+            t=t^a;
         }
-        return -1;
+        return t;
     }
 };
