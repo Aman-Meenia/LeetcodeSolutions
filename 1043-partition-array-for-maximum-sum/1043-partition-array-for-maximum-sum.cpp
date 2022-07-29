aee2456e3640a9f09083cd327f34242eecbca177
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-29 19:30:04 +0530

    Time: 38 ms (24.23%), Space: 8.4 MB (92.03%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,31 +1,60 @@
 class Solution {
 public:
-    int dp[501];
-    int fun(vector<int> & v , int index, int k , int maximum){
-        int ans=INT_MIN;
+//     int dp[501];
+//     int fun(vector<int> & v , int index, int k , int maximum){
+//         int ans=INT_MIN;
 
-        if(index>=v.size()) return 0;
-           if(dp[index]!=-1) return dp[index];
-        for(int i=index; i<v.size(); i++){
+//         if(index>=v.size()) return 0;
+//            if(dp[index]!=-1) return dp[index];
+//         for(int i=index; i<v.size(); i++){
+//             maximum=max(maximum,v[i]);
+//             if(i<(index+k)){
+//  int temp=maximum*((i-index)+1) +fun(v,i+1,k,INT_MIN); 
+//                 ans=max(ans,temp);
+//             }else{
+//                 break;
+//             }
+            
+            
+            
+//         }
+        
+//         return dp[index]=ans;
+        
+        
+        
+//     }
+    int maxSumAfterPartitioning(vector<int>& v, int k) {
+        int n = v.size();
+int dp[n+1];
+        memset(dp,0,sizeof dp);
+        for(int index=n-1; index>=0; index--){
+            int ans =INT_MIN;
+            int maximum =INT_MIN;
+            for(int i=index; i<v.size(); i++){
             maximum=max(maximum,v[i]);
             if(i<(index+k)){
- int temp=maximum*((i-index)+1) +fun(v,i+1,k,INT_MIN); 
+ int temp=maximum*((i-index)+1) +dp[i+1];
+
                 ans=max(ans,temp);
-            }else{
-                break;
             }
-            
-            
-            
+       
         }
         
-        return dp[index]=ans;
+        
+                      dp[index]=ans;
+        }
         
         
         
-    }
-    int maxSumAfterPartitioning(vector<int>& v, int k) {
-        memset(dp,-1,sizeof dp);
-        return fun(v,0,k,INT_MIN);
+        
+        
+        
+        
+        
+        
+        
+        
+        return dp[0];
     }
 };
