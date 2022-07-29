1979a802fc8915925cf21976a65a2c960b951cea
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-29 18:32:17 +0530

    Time: 67 ms (85.51%), Space: 22.2 MB (44.56%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,47 +1,60 @@
 class Solution {
 public:
-bool ispalindrome(int low , int high ,string & s){
-    // cout<<" s "<<low<<" high "<<high<<endl;
-    while(low<=high){
-        if(s[low]!=s[high]){
-            return false;
-        }
-        low++;
-        high--;
-    }
-
-    return true;
-}
     int minCut(string s) {
-        int n = s.size();
-        int dp[n+1];
-memset(dp,0,sizeof dp);
-        
-//         base case
+        int size=s.size();
+int dp[size][size];
+memset(dp,0,sizeof(dp));
 
-             dp[n]=0;
+for(int k=0; k<size; k++){
+	for(int i=0,j=k; j<size; i++,j++){
+		if(k==0){
+dp[i][j]=true;
+		}else if(k==1){
+if(s[i]==s[j]){
+	dp[i][j]=true;
+}else{
+	dp[i][j]=false;
+}
+		}else{
+if(s[i]==s[j]&&dp[i+1][j-1]==1){
+	dp[i][j]=true;
+		}else{
+	dp[i][j]=false;
+}
+	}
+}
 
+
+}
+// for(int i=0; i<size; i++){
+// 	for(int j=0; j<size; j++){
+// 		cout<< dp[i][j]<<"   ";
+// 	}cout<<endl;
+// }
         
-        for(int i=n-1; i>=0; i--){
-   int ans=INT_MAX;
-             for(int k=i; k<s.size(); k++){
-            if(ispalindrome(i,k,s)){
-            int temp=(1+dp[k+1]);
-                ans=min(ans,temp);
+ vector<int> dp1(size,INT_MAX);    
+          dp1[0] = 0;
+        
+          for(int j=1;j<size;j++)
+            {
+              if(dp[0][j]) 
+                  dp1[j] = 0;
+              
+              else{
+                for(int i=j ;i>=1;i--)
+                {
+                    if(dp[i][j])
+                    {
+                        dp1[j] = min(dp1[j],1+dp1[i-1]);
+                    }
+                }
+              }
             }
-        }
-        
-   dp[i]=ans;  
-            
-            
-        }
-        
-        
-        
-        
-        
-        
-        
-         return dp[0]-1;
+
+   
+        return dp1[size-1];
+
+
+
     }
 };
