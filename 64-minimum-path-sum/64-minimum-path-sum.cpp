dfbdbdb148c30a326f2a862430507837b5266f9e
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-07-30 13:18:32 +0530

    Time: 21 ms (23.64%), Space: 10.4 MB (6.02%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,34 +1,30 @@
+
 #define ll long long 
 class Solution {
 public:
-    int dp[201][201];
-    ll fun(vector<vector<int> > & v, int i , int j){
+    int minPathSum(vector<vector<int>>& v) {
         int n = v.size();
         int m = v[0].size();
-        if(i==n-1 && j==m-1){
-            return v[i][j];
-        }
-        
-        
-        if(i>=n || j>=m || i<0 || j<0){
-            return INT_MAX;
-        }
-  if(dp[i][j]!=-1) return dp[i][j];
-        ll sum1= v[i][j]+fun(v,i+1,j);
-        ll sum2 =v[i][j]+fun(v,i,j+1);
+    vector<vector<ll>> dp(n+1,vector<ll>(m+1,INT_MAX));
 
-        return dp[i][j]=min(sum1,sum2);
-        
-        
-    }
-    
-    
-    
-    
-    
-    
-    int minPathSum(vector<vector<int>>& grid) {
-        memset(dp,-1,sizeof dp);
-        return fun(grid,0,0);
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+               if(i==n-1 && j==m-1){
+                   dp[n-1][m-1]=v[n-1][m-1];
+               }else{
+                   ll sum1;
+                   ll sum2;
+                   
+                     sum1= v[i][j]+dp[i+1][j];  
+                   
+                       sum2 =v[i][j]+dp[i][j+1];
+                   
+
+       dp[i][j]=min(sum1,sum2);
+               }
+                
+            }  
+        }
+        return dp[0][0];
     }
 };
