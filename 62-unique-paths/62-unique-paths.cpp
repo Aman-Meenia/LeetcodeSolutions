10950c866db8f4402925286b32b1606b4aaa36ed
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-01 06:56:56 +0530

    Time: 3 ms (44.81%), Space: 5.9 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-   int dp[101][101];
-int fun(int x, int y , int i , int j ){
-    if(i==x-1 && j==y-1) return 1;
-     if(dp[i][j]!=-1){
-        return dp[i][j];
-    }
-    int array1[2]={1,0};
-    int array2[2]={0,1};
-    int sum=0;
     
-for(int k=0; k<2; k++){
-    int a=i+array1[k];
-    int b=j+array2[k];
-    // cout<<a<<" b  "<<b<<endl;
-    if(a>=0 && b>=0 && a<x && b<y){
-        sum+=fun(x,y,a,b);
-    }
-}
-    return dp[i][j]=sum;
-    
-}
     int uniquePaths(int m, int n) {
-        memset(dp,-1,sizeof dp);
-        return fun(m,n,0,0);
+        int dp[m+1][n+1];
+        memset(dp,0,sizeof dp);
+
+        
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+if(i==m-1 && j==n-1){
+    dp[i][j]=1;
+    
+}else{
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+                
+            }
+        }
+
+        return dp[0][0];
     }
 };
