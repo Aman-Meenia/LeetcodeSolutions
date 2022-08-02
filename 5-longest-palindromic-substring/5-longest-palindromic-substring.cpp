b5d1bbce3e241e9965b58a922072ff8472a6727d
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-02 23:56:37 +0530

    Time: 217 ms (53.95%), Space: 23.9 MB (43.84%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,46 +1,46 @@
-class Solution 
-{
+class Solution {
 public:
-    string longestPalindrome(string s)
-    {
-         int n=s.size();
-
-int dp[n][n];
-memset(dp,0,sizeof dp);
-int start=0;
-int maxlength=0;
-for(int i=0; i<n; i++){
-    dp[i][i]=1;
-    start=i;
-    maxlength=1;
-}
-    
-for(int i=0; i<n-1; i++){
-    if(s[i]==s[i+1])
-            {
-                dp[i][i+1]=1;
-                start=i;
-                maxlength=2;
-            }
-}
-for(int j=2; j<n; j++){
-int t=j;
-    for(int i=0; i<n-j; i++){
-                if(s[i]==s[t] && dp[i+1][t-1]==1)
-                {
-                    dp[i][t]=true;
-
-
-                    if(t-i+1>maxlength){
-                        start=i;
-                        maxlength=t-i+1;
-                    }
-                 
+    string longestPalindrome(string s) {
+        
+        int n = s.size();
+        int dp[n+1][n+1];
+        memset(dp,-1,sizeof dp);
+                string ans;
+//           if string is of length 1 it is always palindrome 
+        // string ans;
+        ans.push_back(s[0]);
+        for(int i=0; i<n; i++) dp[i][i]=1;
+        //           if string is of length 2 if it both character are. same then it is  palindrome 
+        
+        for(int i=1; i<n; i++){
+            if(s[i]==s[i-1]){
+                if(ans.size()==1){
+                    ans.clear();
+                     ans.push_back(s[i-1]);
+                     ans.push_back(s[i]);
                 }
-t++;
+                // ans=s[i-1]+s[i];
+                dp[i-1][i]=1;
+            }
+        }
 
-    }
+        
+        for(int j=2; j<n; j++){
+            int k =j;
+            for(int i=0; i<n-j; i++){
+                if(s[i]==s[k] && dp[i+1][k-1]==1){
+                    dp[i][k]=1;
+if(k-i+1>ans.size()){
+    ans=s.substr(i,k-i+1);
 }
-        return s.substr(start,maxlength);
+                }
+                
+                k++;
+            }
+        }
+        
+        
+        return ans;
+        
     }
 };
