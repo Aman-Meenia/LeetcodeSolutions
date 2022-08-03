be694023b43228b1eb531b344b5c45b9bf0ee1e0
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-03 07:12:31 +0530

    Time: 13 ms (38.41%), Space: 8.3 MB (61.53%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -2,34 +2,58 @@ class Solution {
 public:
     
     
-int dp[31][3001];
-    bool fun(vector<int> & v , int index ,int target){
-        if(target==0) return true;
-        if(index>=v.size()) return false;
-   if(dp[index][target]!=-1) return dp[index][target];
-        int a = false , b = false;
+// int dp[31][3001];
+//     bool fun(vector<int> & v , int index ,int target){
+//         if(target==0) return true;
+//         if(index>=v.size()) return false;
+//    if(dp[index][target]!=-1) return dp[index][target];
+//         int a = false , b = false;
         
-        if(v[index]<=target){
-       a = fun(v,index+1,target-v[index]);
-        }
-       b = fun(v,index+1,target);
-        return  dp[index][target]=a||b;
-    }
+//         if(v[index]<=target){
+//        a = fun(v,index+1,target-v[index]);
+//         }
+//        b = fun(v,index+1,target);
+//         return  dp[index][target]=a||b;
+//     }
     
     
     
     int lastStoneWeightII(vector<int>& v) {
         int sum = 0;
         for(auto a:v) sum+=a;
-        for(int i=sum/2; i>=0; i--){
-            memset(dp,-1,sizeof dp);
-            if(fun(v,0,i)){
-                int a = sum-i;
-                int b = i;
-                return abs(a-b);
+        int n = v.size();
+        int dp[n+1][sum+1];
+        for(int k=sum/2; k>=0; k--){
+memset(dp,0,sizeof dp);
+            for(int i=0; i<n; i++){
+                dp[i][0]=1;
+            }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                bool a =false , b=false;
+                if(v[i-1]<=j){
+                   a=dp[i-1][j-v[i-1]];
+                }
+                   b=dp[i-1][j];
+                dp[i][j]=a||b;
             }
             
+            
+            
         }
-        return -1;
+            if(dp[n][k]==1) {
+                int a = sum-k;
+                int b = k;
+                return abs(a-b);
+            }
+        
+        }
+        
+        
+        
+        
+        
+        
+        return sum;
     }
 };
