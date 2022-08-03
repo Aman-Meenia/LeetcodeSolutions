44d8eea79a7ecbd30d217860d68bfcdaab46bab3
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-03 17:38:51 +0530

    Time: 0 ms (100.00%), Space: 6 MB (17.25%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -2,9 +2,19 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        if(num==1) return true;
-    ll t = sqrt(num);
-        if(t*t==num) return true;
+
+        int low = 0;
+        int high = num;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if((ll)mid*(ll)mid == num){
+                return true;
+            }else if((ll)mid*(ll)mid < num){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
         return false;
     }
 };
