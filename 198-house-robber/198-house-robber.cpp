ccff7e1e25818944f235c0f70e028d01d20bbc5e
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-04 06:57:48 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.61%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    int dp[101][2];
-    int fun(vector<int> & v, int index,  int cnd){
-        if(index>=v.size()) return 0;
-        int a=0 , b=0; 
-        if(dp[index][cnd]!=-1) return dp[index][cnd];
-        if(cnd==true){
-           a= fun(v,index+1,false);
-        }else{
-           a=v[index]+fun(v,index+1,true);
-          b=  fun(v,index+1,false);
+//     int dp[101][2];
+//     int fun(vector<int> & v, int index,  int cnd){
+//         if(index>=v.size()) return 0;
+//         int a=0 , b=0; 
+//         if(dp[index][cnd]!=-1) return dp[index][cnd];
+//         if(cnd==true){
+//            a= fun(v,index+1,false);
+//         }else{
+//            a=v[index]+fun(v,index+1,true);
+//           b=  fun(v,index+1,false);
+            
+//         }
+//         return dp[index][cnd]=max(a,b);
+//     }
+    int rob(vector<int>& v) {
+        int n = v.size();
+        int dp[n+2];
+        memset(dp,0,sizeof dp);
+        // int cnd = true;
+        for(int i=n-1; i>=0; i--){
+           
+int  a = max(v[i]+dp[i+2],dp[i+1]);
+            
+            dp[i]=a;
             
         }
-        return dp[index][cnd]=max(a,b);
-    }
-    int rob(vector<int>& v) {
-        memset(dp,-1,sizeof dp);
-        return fun(v,0,false);
+        
+        
+        return dp[0];
     }
 };
