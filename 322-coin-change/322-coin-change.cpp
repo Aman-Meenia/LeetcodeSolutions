1a4ba1bece9df20ef9dcbe8cd38371c398ce0432
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-04 16:21:55 +0530

    Time: 97 ms (81.81%), Space: 10.5 MB (91.01%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,32 +1,49 @@
 class Solution {
 public:
    
-    int dp[14][10001];
-int fun(vector<int> & v, int index,int target){
-        if(v.size()<=index) return 1e8;
-        if(target==0){
+//     int dp[14][10001];
+// int fun(vector<int> & v, int index,int target){
+//         if(v.size()<=index) return 1e8;
+//         if(target==0){
         
-            return 0;
-        }
-    if(dp[index][target]!=-1) return dp[index][target];
-        // cout<<" target "<<target<<endl;
-        if(v[index]<=target){
-           return dp[index][target]= min(1+fun(v,index,target-v[index]), fun(v,index+1,target));
+//             return 0;
+//         }
+//     if(dp[index][target]!=-1) return dp[index][target];
+//         // cout<<" target "<<target<<endl;
+//         if(v[index]<=target){
+//            return dp[index][target]= min(1+fun(v,index,target-v[index]), fun(v,index+1,target));
      
-        }else{
-            return dp[index][target]= fun(v,index+1,target);
-        }
+//         }else{
+//             return dp[index][target]= fun(v,index+1,target);
+//         }
        
-    }
+//     }
     
     
     int coinChange(vector<int>& v, int amount) {
-        if (amount == 0 ) return 0;
-                memset(dp,-1,sizeof dp);
-int ans = fun(v,0,amount);
+        int n = v.size();
 
-        if(ans==1e8) return -1;
-        return ans;
+        int dp[n+1][amount+1];
+             memset(dp,0,sizeof dp);
+          for(int i=0; i<amount+1; i++){
+        dp[0][i]=INT_MAX-1;
+        
+    }
+       
+        for(int i=1; i<=n; i++){
+      for(int j=1; j<=amount; j++){
+      if(v[i-1]<=j){
+                dp[i][j]=min(dp[i][j-v[i-1]]+1,dp[i-1][j]);
+            }else{
+                dp[i][j]=dp[i-1][j];
+            }
+    }
+        }
+        
+        
+
+      if(dp[n][amount]==INT_MAX-1) return -1;
+        return dp[n][amount];
     
     }
 };
