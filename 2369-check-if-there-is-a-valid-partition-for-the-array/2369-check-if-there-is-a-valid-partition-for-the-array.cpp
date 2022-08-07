af1a3984140e23f5f9b54236a5bf793f8ce35894
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-07 11:01:44 +0530

    Time: 252 ms (55.56%), Space: 84 MB (22.22%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     
-    
-int dp[100000];
-    bool fun(vector<int> & v ,int i){
-        int size = v.size();
-        if(i>=size){
-            return true;
-        }
-        if(dp[i]!=-1) return dp[i];
-        int ans = false;
-        if(i+1<size && v[i]==v[i+1]){
-           ans = ans | fun(v,i+2);
-        }
-        if(i+2<size && v[i]==v[i+1] && v[i+1]==v[i+2]){
-            ans=ans|fun(v,i+3);
-        }
-        if(i+2<size && v[i]+1==v[i+1] && v[i+1]+1==v[i+2]){
-            ans=ans|fun(v,i+3);
-        }
-        return dp[i]=ans;
-        
-    }
-    
-    
-    
     bool validPartition(vector<int>& v) {
-        memset(dp,-1,sizeof dp);
-        return fun(v,0);
+        
+int n = v.size();
+        int dp[n+1];
+                memset(dp,0,sizeof dp);
+        dp[n]=1;
+        for(int i=n-1; i>=0; i--){
+            int ans = 0;
+             if(i+1<n && v[i]==v[i+1]){
+
+           ans = ans | dp[i+2];
+        }
+        if(i+2<n && v[i]==v[i+1] && v[i+1]==v[i+2]){
+
+            ans=ans|dp[i+3];
+        }
+        if(i+2<n && v[i]+1==v[i+1] && v[i+1]+1==v[i+2]){
+
+
+            ans=ans|dp[i+3];
+        }
+            
+            dp[i]=ans;
+        }
+        
+        
+        
+        
+        
+        return dp[0];
     }
 };
