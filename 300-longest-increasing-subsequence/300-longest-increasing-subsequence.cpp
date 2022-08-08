1e09c06aa4685af544f752d18936149336aabf8f
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-08 06:57:25 +0530

    Time: 329 ms (62.40%), Space: 10.4 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int fun(vector<int> & v){
+    int lengthOfLIS(vector<int>& v) {
         int n = v.size();
-vector<int> dp(n+1,1);
-        int ans=0;
+        vector<int> dp(n,1);
+        int ans = 0;
         for(int i=0; i<n; i++){
-            // cout<<" v[i] "<<v[i]<<endl;
-            for(int j=0; j<i; j++){
-                // cout<<v[j]<<" ";
-                if(v[i]>v[j]){
-                    dp[i]=max(dp[j]+1,dp[i]);
-                }
-            }
 
-        }
-        for(auto a:dp){
-            // cout<<a<<" "<<endl;
-        ans=max(ans,a);
+              for(int j=0; j<i; j++){
+            
+                  if(v[j]<v[i]){
+                      dp[i]=max(dp[i],dp[j]+1);
+                  }
+              
+              
+              }
+            ans=max(ans,dp[i]);
+            
             }
         return ans;
         
-    }
-    int lengthOfLIS(vector<int>& nums) {
         
-        return fun(nums);
+        
     }
 };
