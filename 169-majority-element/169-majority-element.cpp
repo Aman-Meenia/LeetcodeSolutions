a89b71dfe2fc97c804d93c70f5f34dc1a374061f
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-12 15:19:10 +0530

    Time: 35 ms (37.68%), Space: 19.4 MB (99.55%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& v) {
-        int ans=v[0];
-        int cnt=1;
-        int n=v.size();
+     int cnt = 1;
+        int n = v.size();
+        int num = v[0];
         for(int i=1; i<n; i++){
-            if(ans==v[i]){
+            if(num==v[i]){
                 cnt++;
             }else{
                 cnt--;
                 if(cnt==0){
+                    num=v[i];
                     cnt=1;
-                    ans=v[i];
                 }
             }
         }
-        return ans;
+        return num;
+        
     }
 };
