8a8e01ee516e3bd8131f5807d4ca64f5fb07cb27
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-22 06:51:15 +0530

    Time: 3 ms (58.67%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -3,7 +3,10 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
 while(n>1){
-      if(n%4) return false;
+      if(n%4>=1){
+         
+          return false;
+      } 
     n=n/4;
 }
         return n==1;
