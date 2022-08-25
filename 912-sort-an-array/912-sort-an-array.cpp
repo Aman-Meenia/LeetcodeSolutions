16a05e58c7e47cb2a90a6ca12290c48745cc2835
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-25 14:23:28 +0530

    Time: 118 ms (44.40%), Space: 39 MB (26.04%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     
+//     void shellsort(vector<int> & v ){
+//         int n = v.size();
+//         for(int gap=n/2; gap>=1; gap=gap/2){
+//             for(int j=gap; j<n; j++){
+//                 for(int i=j-gap; i>=0; i=i-gap){
+//                     if(v[i+gap]>v[i]){
+//                         break;
+//                     }else{
+//                         swap(v[i+gap],v[i]);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+    
     void shellsort(vector<int> & v ){
-        int n = v.size();
-        for(int gap=n/2; gap>=1; gap=gap/2){
-            for(int j=gap; j<n; j++){
-                for(int i=j-gap; i>=0; i=i-gap){
-                    if(v[i+gap]>v[i]){
-                        break;
-                    }else{
-                        swap(v[i+gap],v[i]);
-                    }
+int n = v.size();
+
+    for(int gap=n/2; gap>=1; gap/=2){
+        for(int j=gap; j<n; j++){
+            for(int i=j-gap; i>=0; i-=gap){
+                if(v[i+gap]>v[i]){
+                    break;
+                }else{
+                    swap(v[i+gap],v[i]);
                 }
             }
         }
     }
-    
+}
     
     
     
