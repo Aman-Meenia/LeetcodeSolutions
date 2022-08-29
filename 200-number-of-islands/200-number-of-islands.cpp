0bc5e250fb34a188b35434f6f115d21f8c2d7d51
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-29 07:22:35 +0530

    Time: 51 ms (60.79%), Space: 19.2 MB (9.36%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,52 +1,51 @@
 class Solution {
 public:
     
-    void fun(vector<vector<char> > & v, int a , int b , vector<vector<bool> > & vis){
+    void fun(vector<vector<char> > & v, vector<vector<int> > & vis , int a ,int b){
         int n = v.size();
-        int m =v[0].size();
-        
-        vis[a][b]=true;
-        int array1[4]={0,0,1,-1};
-        int array2[4]={1,-1,0,0};
-        
-        
-        for(int k=0; k<4; k++){
-            int x = a + array1[k];
-            int y = b + array2[k];
-            if(x>=0 && y>=0 && x<n && y<m && vis[x][y]==false && v[x][y]=='1'){
-                fun(v,x,y,vis);
+        int m = v[0].size();
+        queue<pair<int,int> > q;
+        q.push({a,b});
+        vis[a][b]=1;
+int array1[4]={0,0,-1,1};
+int array2[4]={-1,1,0,0};
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int a1 = q.front().first;
+                int a2 = q.front().second;
+                q.pop();
+                for(int k=0; k<4; k++){
+                int x=a1+array1[k];
+                    int y = a2+array2[k];
+                         
+                    if(x>=0 && y>=0 && x<n && y<m &&v[x][y]=='1' && vis[x][y]==0){
+                        q.push({x,y});
+                        vis[x][y]=1;
+                    }
+                }
             }
         }
-        
-        
+            
+            // cout<<" NEXT CALL "<<endl;
         
     }
     
     
-    
-    
-    
     int numIslands(vector<vector<char>>& v) {
         int n = v.size();
-        int m =v[0].size();
-        
-        vector<vector<bool> > vis(n,vector<bool>(m,false));
-        int ans =0;
-        
+        int m = v[0].size();
+        vector<vector<int> > vis(n,vector<int>(m,0));
+        int cnt=0;
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-            if(vis[i][j]==false && v[i][j]=='1'){
-                ans++;
-                fun(v,i,j,vis);
-            }
-            }
+for(int j=0; j<m; j++){
+    if(vis[i][j]==0 && v[i][j]=='1'){
+        fun(v,vis,i,j);
+     cnt++;   
+    }
+}
         }
         
-        
-        
-        
-        
-        return ans;
-        
+return cnt ;
     }
 };
