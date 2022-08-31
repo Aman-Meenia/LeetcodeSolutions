976f9fc438e3b2c96893d7144207e3637fd375ed
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-08-31 07:35:01 +0530

    Time: 46 ms (91.63%), Space: 19.3 MB (42.09%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -2,71 +2,69 @@ class Solution {
 public:
     
     
-    void fun(vector<vector<int> > & v, int a , int b ,vector<vector<bool> > &visited){
-          int n = v.size();
+    void fun(vector<vector<int> > & v, vector<vector<int> > & visited , int a , int b){
+        int n = v.size();
         int m = v[0].size();
-    visited[a][b]=true;
-  int array1[4]={1,-1,0,0};
-        int array2[4]={0,0,-1,1};
-
-
+        visited[a][b]=1;
+        int array1[4]={0,0,1,-1};
+        int array2[4]={1,-1,0,0};
         for(int k=0; k<4; k++){
-
-       int x = a + array1[k];
-       int y = b + array2[k];
-   if(x>=0 && y>=0 && x<n && y<m && visited[x][y]==false && v[x][y]>=v[a][b]){
- 
-fun(v,x,y,visited);
-       }
-       
-
+            
+            
+            int x=a+array1[k];
+            int y=b+array2[k];
+            
+            if(x>=0 && y>=0 && x<n && y<m && v[x][y]>=v[a][b] &&visited[x][y]==0){
+                fun(v,visited,x,y);
+            }
         }
         
-
-
-}
-
+        
+        
+        
+    }
+    
     
     
     
     
     
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& v) {
-        vector<vector<int> > ans;
-        int n = v.size();
-        int m = v[0].size();
-        vector<vector<bool> > pacific(n,vector<bool>(m,false));
-           vector<vector<bool> > atlantic(n,vector<bool>(m,false));
         
-        // fun(v,1,m-1,atlantic);
+       int n = v.size();
+        int m= v[0].size();
+        
+    vector<vector<int> > pacific(n,vector<int>(m,0));
+     vector<vector<int> > atlantic(n,vector<int>(m,0));
+        
+        
+//          WE DO THIS QUESTION IN REVERSE ORDER AND CHECK FROM THE LAST ROW, START ROW , LAST COLUMN AND START COULMN UPTO WHICH POINT WE REACH THROUGH THESE POINT 
+//          CHECK FOR START ROW (PACIFIC OCEAN)
+        for(int i=0; i<m; i++){
+            fun(v,pacific,0,i);
+        }
+ //          CHECK FOR LEFT CLOUMN (PACIFIC OCEAN)
         for(int i=0; i<n; i++){
-            fun(v,i,m-1,atlantic);
-        }
-        for( int j=0; j<m; j++){
-            fun(v,n-1,j,atlantic);
+            fun(v,pacific,i,0);
         }
         
-            for(int i=0; i<n; i++){
-            fun(v,i,0,pacific);
+  //          CHECK FOR START COULMN (ATLANTIC  OCEAN)
+        for(int i=0; i<m; i++){
+            fun(v,atlantic,n-1,i);
         }
-        for(int j=0; j<m; j++){
-            fun(v,0,j,pacific);
+ //          CHECK FOR LEFT CLOUMN (ATLANTIC OCEAN)
+        for(int i=0; i<n; i++){
+            fun(v,atlantic,i,m-1);
         }
+        vector<vector<int> > ans;
         
-       
-        
-        
-      for(int i=0; i<n; i++){
-          
-          for(int j=0; j<m; j++){
-              
-              if(pacific[i][j]==true && atlantic[i][j]==true){
-                  ans.push_back({i,j});
-              }
-          }
-      }  
-        
-     return ans;   
-        
+        for(int i=0; i<n; i++){
+for(int j=0; j<m; j++){
+    if(pacific[i][j]==1 && atlantic[i][j]==1){
+ans.push_back({i,j});
+    }
+}
+        }
+        return ans;
     }
 };
