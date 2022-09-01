6a663ae91fcd104f0d95f7793a8580fcf55195ee
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-09-01 18:12:53 +0530

    Time: 792 ms (5.14%), Space: 31.5 MB (61.08%) - LeetHub

--- 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.cpp
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.cpp
@@ -2,20 +2,91 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& v, int target) {
         int n = v.size();
-        unordered_map<int,int>mp;
-        int sum = 0;
-        mp[0]=1;
-       int ans = 0;
-        for(int i=0; i<n; i++){
-            sum+=v[i];
-            int k = sum-target;
-            if(mp.find(k)!=mp.end()){
-                ans+=mp[k];
-            }
-            mp[sum]+=1;
-        }
-        return ans;
-    
         
+        if(target==0){
+            int ans = 0;
+            int cnt = 0;
+            for(int i=0; i<n; i++){
+                if(v[i]==0) cnt++;
+                else{ 
+                    
+                ans+=cnt*(cnt+1)/2;
+                    cnt=0;
+                    }
+            }
+             ans+=cnt*(cnt+1)/2;
+                    cnt=0;
+
+            return ans;
+        }
+        
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+        vector<int> t;
+        for(int i=0; i<n; i++){
+            if(i==0){
+if(v[i]==0) left[i]=1;
+                else t.push_back(i);
+            }else{
+                if(v[i]==0){
+left[i]=left[i-1]+1;
+                    
+                }else t.push_back(i);
+            }
+        }
+             for(int i=n-1; i>=0; i--){
+            if(i==n-1){
+if(v[i]==0) right[i]=1;
+      
+            }else{
+                if(v[i]==0){
+right[i]=right[i+1]+1;
+                    
+                }
+            }
+        }
+        
+        for(auto a:left){
+            cout<<a<<" ";
+        }cout<<endl;
+        for(auto a:right){
+            cout<<a<<" ";
+        }cout<<endl;
+        int size = t.size();
+        int low = 0;
+        int high = 0;
+        int sum =0;
+        int ans = 0;
+        while(high<size){
+            sum+=v[t[high]];
+            if(sum>target){
+                while(sum>target){
+                    sum-=v[t[low]];
+                    low++;
+                }
+            }
+int start = t[low];
+            int end = t[high];
+            cout<<" start "<<start<<" end "<<end<<endl;
+            if(sum==target){
+                int t1 = 1;
+                int t2 = 1;
+            if(start!=0){
+                t1+=left[start-1];
+            }
+            if(end!=n-1){
+              t2+=right[end+1];
+            }
+       ans+=t1*t2;
+            }
+            high++;
+        }
+        
+        
+        
+        
+        
+        
+        return ans;
     }
 };
