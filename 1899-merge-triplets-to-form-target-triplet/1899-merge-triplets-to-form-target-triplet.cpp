0e43c719cd2d92e053d6b14a0472b89c89448431
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-09-03 15:37:55 +0530

    Time: 1083 ms (14.72%), Space: 146.5 MB (71.27%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& v, vector<int>& target) {
+        int n = v.size();
+        vector<int> t(3,0);
+        for(int i=0; i<n; i++){
+if(v[i][0]<=target[0] && v[i][1]<=target[1] && v[i][2]<=target[2]){
+t[0]=max(t[0],v[i][0]);
+    t[1]=max(t[1],v[i][1]);
+    t[2]=max(t[2],v[i][2]);
+    
+}
+            
+            
+        }
+        for(auto a:t){
+            cout<<a<<" ";
+        }cout<<endl;
+        if(t==target){
+            return true;
+        }
+        return false;
+        
+    }
+};
