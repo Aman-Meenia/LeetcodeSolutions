ee2d32eb61f517b52dbe89d7a393e3647f911611
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-09-04 13:36:13 +0530

    Time: 0 ms (100.00%), Space: 6 MB (43.48%) - LeetHub

--- 400-nth-digit/400-nth-digit.cpp
+++ 400-nth-digit/400-nth-digit.cpp
@@ -1,79 +1,74 @@
 #define ll long long
-class Solution {
+class Solution
+{
 public:
-    
-    
-    int fun(int start , int end , int target, int size){
+    int fun(int start, int end, int target, int size)
+    {
         int ans = 0;
-                                     
 
-if(target%size==0){
-        int temp = target/size;
-    start+=temp;
-start-=1;
-    return start%10;
-}else{
-    int temp = target/size;
+        if (target % size == 0)
+        {
+            int temp = target / size;
+            start += temp;
+            start -= 1;
+            return start % 10;
+        }
+        else
+        {
+            int temp = target / size;
 
-    start+=temp;
+            start += temp;
+
+            int t = target % size;
+            string s = to_string(start);
+
+            return s[t - 1] - '0';
 
 
-    int t = target%size;
-    string s = to_string(start);
+        }
 
-    return s[t-1]-'0';
-
-return ans;
-}
-                                                        return ans;
-        
     }
-    
-    int findNthDigit(int n) {
-        
+
+    int findNthDigit(int n)
+    {
+
         vector<ll> v;
-       for(int i=1; i<=9; i++){
-           ll x = 9;
-           if(i>0){
-               x=x*i;
-               int t = i-1;
-               while(t--){
-                   x=x*10;
-               }
-           }
-           v.push_back(x);
-       }
-        for(auto a:v){
-            cout<<a<<" ";
-        }cout<<endl;
-        
-int N = n;
-for(int i=0; i<v.size(); i++){
-   
-    ll start = 1;
-    ll end = 9;
-    ll t = i;
-    while(t--){
-        start*=10;
-        end*=10;
-        end+=9;
-    }
+        for (int i = 1; i <= 9; i++)
+        {
+            ll x = 9;
+            if (i > 0)
+            {
+                x = x * i;
+                int t = i - 1;
+                while (t--)
+                {
+                    x = x * 10;
+                }
+            }
+            v.push_back(x);
+        }
 
-    if(n<=v[i]){
- 
 
-      return fun(start,end,n,i+1) ;
-    }
-    n-=v[i];
+        for (int i = 0; i < v.size(); i++)
+        {
 
-    
-}
-        
-        
-        
-        
-        
-        
-       return 0; 
+            int start = 1;
+            int end = 9;
+            int t = i;
+            while (t--)
+            {
+                start *= 10;
+                end *= 10;
+                end += 9;
+            }
+
+            if (n <= v[i])
+            {
+                return fun(start, end, n, i + 1);
+            }
+            n -= v[i];
+        }
+
+        return 0;
     }
 };
