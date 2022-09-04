4571caae83cd785aeda57dd8a57a49902dc19ac5
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-09-04 11:31:07 +0530

    Time: 3 ms (72.21%), Space: 6 MB (25.45%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,30 @@
+#define ll long long
 class Solution {
 public:
+    
+bool fun(int mid,int target){
+    if((ll)mid*mid<=target){
+        return true;
+    }
+    return false;
+}
+    
+    
+    
+    
     int mySqrt(int x) {
-        return sqrt(x);
+        int low = 0;
+        int high = INT_MAX;
+        int ans = -1;
+        while(low<=high){
+            int mid = low +(high-low)/2;
+            if(fun(mid,x)){
+                ans = mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans ;
     }
 };
