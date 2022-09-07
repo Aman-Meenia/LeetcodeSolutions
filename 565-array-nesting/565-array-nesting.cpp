e352f4f1cfed098e9f536c76e4714daf236ee675
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-09-08 00:22:50 +0530

    Time: 325 ms (23.39%), Space: 94.7 MB (43.83%) - LeetHub

--- 565-array-nesting/565-array-nesting.cpp
+++ 565-array-nesting/565-array-nesting.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int arrayNesting(vector<int>& v) {
         int n = v.size();
-        unordered_set<int> st;
+      vector<int> st(n,0);
         int ans = 0;
-        // unordered_map<int,int> mp;
+
         for(int i=0; i<n; i++){
             int cnt = 1;
 int temp = v[i];
-            st.insert(i);
-            while(st.find(temp)==st.end()){
-                // cout<<temp<<"  ";
+            st[i]=1;
+            while(st[temp]!=1){
                 cnt++;
-                st.insert(temp);
+                st[temp]=1;
                 temp = v[temp];
             }
-            // cout<<endl;
+
             ans = max(ans,cnt);
         }
         return ans;
