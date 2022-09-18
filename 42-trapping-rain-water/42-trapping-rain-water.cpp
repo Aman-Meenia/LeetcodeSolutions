f9135c057917a37bb849ae24bc9000bf9d7eded3
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-09-18 11:17:35 +0530

    Time: 32 ms (32.63%), Space: 20.5 MB (25.17%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,37 +2,33 @@ class Solution {
 public:
     int trap(vector<int>& v) {
         int n = v.size();
-        int low = 1;
-        int high = n-2;
-        int ans = 0;
-        int startval=v[0];
-        int endval=v[n-1];
-        while(low<=high){
-
-            if(startval<=endval){     
-          startval=max(startval,v[low]);
-            int t =min(startval,endval);
-                if(t>v[low])
-            ans+=t-v[low];
-              low++;
-           
-            
-        }else{
-            endval=max(endval,v[high]);
-               int t =min(startval,endval);
-                  if(t>v[high])
-            ans+=t-v[high];
-                  
-              high--;
-               
-              
-            
+    vector<int> left(n);
+        left[0]=v[0];
+        for(int i=1; i<n; i++){
+left[i]=max(v[i],left[i-1]);
         }
-            
-            
-            
-            
+         vector<int> right(n);
+        right[n-1]=v[n-1];
+        for(int i=n-2; i>=0; i--){
+right[i]=max(v[i],right[i+1]);
         }
-        return ans;
+        
+        // for(auto a:left){
+        //     cout<<a<<" ";
+        // }cout<<endl;
+        // for(auto a:right){
+        //     cout<<a<<" ";
+        // }cout<<endl;
+int ans= 0;
+        for(int i=0; i<n; i++){
+            int temp = v[i];
+            int minimum = min(left[i],right[i]);
+if(minimum>temp){
+    ans+=(minimum-temp);
+}
+        }
+       return ans; 
+        
+        
     }
 };
