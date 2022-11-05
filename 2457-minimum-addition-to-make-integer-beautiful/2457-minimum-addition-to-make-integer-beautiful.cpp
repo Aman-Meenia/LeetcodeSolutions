276ec7acacc9aa34f7ae4fb1df2eaedf1ae5e8be
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-11-05 16:39:05 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (29.87%) - LeetHub

---
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
@@ -0,0 +1,44 @@
+#define ll long long
+class Solution {
+public:
+    long long makeIntegerBeautiful(long long n, int target) {
+        ll temp = n;
+        vector<int> v;
+   
+        ll sum = 0;
+        while(temp>0){
+    
+            int t =temp%10;
+                  sum+=t;
+            v.push_back(t);
+            temp=temp/10;
+        }
+        int k = 0;
+        for(auto a:v){
+            cout<<a<<" ";
+        }cout<<endl;
+
+        while(sum>target){
+
+            sum-=v[k];
+            cout<<" Sum "<<sum<<endl;
+               v[k]=0;
+            if(k==v.size()-1){
+                v.push_back(1);
+            }else{
+                sum+=1;
+            v[k+1]+=1;
+            }
+            k++;
+            
+        }
+        
+        
+      ll ans = 0;
+        for(int i=v.size()-1; i>=0; i--){
+            ans=ans*10+v[i];
+        }
+        cout<<"ans "<<ans<<endl;
+       return ans-n; 
+    }
+};
