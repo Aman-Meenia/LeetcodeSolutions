e8a4d3b86194a03432e41dcfcd6dd906e3e18fab
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-11-07 14:55:08 +0530

    Time: 300 ms (82.48%), Space: 40 MB (88.49%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,87 +1,44 @@
-class TrieNode{
-public:
-    TrieNode *children[26];
-    bool End_Of_Word;
-
-
-    TrieNode(){
-        for(int i=0; i<26; i++){
-            children[i]=NULL;
-        }
-        End_Of_Word=false;
-    }
-
-
-    void insertword(TrieNode * root , string s){
-
-TrieNode * temp = root;
-
-for(int i=0; i<s.size(); i++){
-    char ch = s[i];
-    if(temp->children[ch-'a']==NULL){
-        TrieNode *n = new TrieNode();
-        temp->children[ch-'a']=n;
-    }
-    temp=temp->children[ch-'a'];
-}
-temp->End_Of_Word=true;
-
-
-    }
-
-
-bool searchword(TrieNode *root , string s, int cnt){
-    TrieNode * temp = root;
-    // cout<<s<<endl;
-    for (int i = 0; i <s.size(); ++i)
-    {
-        char ch = s[i];
-        if(temp->children[ch-'a']==NULL) {
-            return false;
-        }
-        temp = temp->children[ch-'a'];
-        if(temp->End_Of_Word==true) {
- 
-            if(i==s.size()-1 && cnt>0) return true;
-            else if(i==s.size()-1 && cnt==0) return false;
-            // cout<<"Aman Meenia "<<endl;
-
-            if(searchword(root,s.substr(i+1),cnt+1)) return true;
-        }
-
-    }
-    
-    return false;
-}
-
-
-
-
-
-};
-
-
-
 class Solution {
 public:
-  
+    
+    
+    unordered_set<string> st;
+    
+    
+    bool search(string s , int n , int cnt){
+        // if(s.size()==0) return true;
+        string temp ;
+        // cout<<" s "<<s<<endl;
+        for(int i=n; i<s.size(); i++){
+            temp.push_back(s[i]);
+            if(st.find(temp)!=st.end()){
+                
+                if(i==s.size()-1 && cnt>0) return true;
+                if(i==s.size()-1 && cnt==0) return false;
+                if(search(s.substr(i+1),0,cnt+1)){
+                    return true;
+                }
+                
+                
+            }
+        }
+        return false;
+    }
+    
+    
+    
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         vector<string> ans;
-        TrieNode *t1 = new TrieNode();
-        
         for(auto a:words){
-            t1->insertword(t1,a);
+st.insert(a);
         }
         for(auto a:words){
-            if(t1->searchword(t1,a,0)){
+            if(search(a,0,0)){
                 ans.push_back(a);
             }
         }
-//         if(t1->searchword(t1,t1,"cats")){
-//     cout<<"PRESENT"<<endl;
-// }else{
-//     cout<<"NOT PRESENT "<<endl;
-// }
         return ans;
+        
+        
     }
 };
