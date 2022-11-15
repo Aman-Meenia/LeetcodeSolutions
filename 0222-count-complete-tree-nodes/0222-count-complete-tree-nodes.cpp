9afd887946cab401237b8173aa2fff6898f33340
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-11-15 06:50:16 +0530

    Time: 45 ms (79.56%), Space: 31.5 MB (7.44%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return 0;
+        queue<TreeNode *>q;
+        q.push(root);
+        int ans =0;
+        while(!q.empty()){
+            int size = q.size();
+            ans+=size;
+            for(int i=0; i<size; i++){
+                TreeNode * n = q.front();
+                q.pop();
+                if(n->left){
+                    q.push(n->left);
+                }
+                if(n->right){
+                    q.push(n->right);
+                }
+            }
+            
+            
+            
+            
+            
+            
+            
+        }
+        return ans; 
+    }
+};
