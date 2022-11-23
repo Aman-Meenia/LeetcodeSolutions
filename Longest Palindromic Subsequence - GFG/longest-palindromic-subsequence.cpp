5cb15a35f2af1e51087b6ac8d96e633cfb35ced3
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-11-23 19:11:48 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -1,39 +1,46 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial template for C++
 
 #include<bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function Template for C++
 
 class Solution{
   public:
-    int longestPalinSubseq(string s1) {
-      string s2=s1;
-reverse(s2.begin(),s2.end());
-int n=s1.size();
+  
+int dp[1001][1001];
 
-int dp[n+1][n+1];
-memset(dp,0,sizeof dp);
-for(int i=1; i<=n; i++){
-    for(int j=1; j<=n; j++){
-if(s1[i-1]==s2[j-1]){
-    dp[i][j]=1+dp[i-1][j-1];
-    }else{
-        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-    }
-}
-}
+int fun(string s , string t , int i , int j){
+      if(i>=j){
+          if(i==j) return 1;
+          return 0;
+      }
+      if(dp[i][j]!=-1) return dp[i][j];
+      int ans1 =0, ans2= 0, ans3=0;
+      if(s[i]==t[j]){
+return dp[i][j]=2+fun(s,t,i+1,j-1);
+      }else{
+        ans2= fun(s,t,i,j-1);
+        ans3= fun(s,t,i+1,j);
+return dp[i][j]=max(ans2,ans3);
+      }
+             
+ 
+  }
 
-return dp[n][n];
-        
-        //code here
+
+    int longestPalinSubseq(string A) {
+  int size = A.size();
+  string s =A;
+  memset(dp,-1,sizeof dp);
+ return fun(A,s,0,size-1);
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int32_t main()
 {
@@ -46,4 +53,5 @@ int32_t main()
     }
 }
 // Contributed By: Pranay Bansal
-  // } Driver Code Ends
+
+// } Driver Code Ends
