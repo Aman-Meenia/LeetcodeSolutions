ef559ae72b761aaf7aa50ca0ff83f3ee3f681b48
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-11-26 10:32:04 +0530

    Time: 1480 ms (13.50%), Space: 98.3 MB (95.83%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -1,54 +1,46 @@
 #define ll long long
 class Solution {
 public:
-    long long maxTaxiEarnings(int n, vector<vector<int>>& v) {
+    static int cmp(vector<int> &a , vector<int> &b){
+    if(a[1]==b[1]) return a[0]<b[0];
+        return a[1]<b[1];
+    }
+    long long maxTaxiEarnings(int N, vector<vector<int>>& v) {
        
-        sort(v.begin(),v.end());
-      priority_queue<ll,vector<ll>  > maxpq;
-priority_queue<pair<ll,ll> ,vector<pair<ll,ll>> ,greater<pair<ll,ll> > > pq;  
-int size = v.size();
-//         for(int i=1; i<n; i++){
-//             ll profit = v[i][1]-v[i][0]+v[i][2];
-//          while(!pq.empty() && pq.top().first()<=v[i][0]){
-//                 maxpq.push(pq.top().second);
-//                 pq.pop();
-//             }   
-            
-            
-            
-// if(!maxpq.empty()){
-//     profit+=maxpq.top();
-// }
-            
-//             pq.push_back({v[i][1],profit});
-//         }
-        int t = 0;
-        for(ll i=0; i<n; i++){
-while(!pq.empty() && pq.top().first<=i){
-                maxpq.push(pq.top().second);
-                pq.pop();
+        sort(v.begin(),v.end(),cmp);
+  //       for(auto a:v){
+  // cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<endl;
+  //       }
+        int n = v.size();
+        vector<ll> dp(n,0);
+        dp[0]=v[0][2]+v[0][1]-v[0][0];
+        for(int i=1; i<n; i++){
+           
+            int low = 0;
+            int high = i-1;
+            int previous = -1;
+            while(low<=high){
+               int mid = low +(high-low)/2;
+                if(v[mid][1]<=v[i][0]){
+                    previous= mid;
+                    low = mid+1;
+                }else { 
+                high = mid-1;
+                }
             }
-while(t<size && v[t][0]<=i){
-    ll profit = v[t][1]-v[t][0]+v[t][2];
-if(!maxpq.empty()){
-    profit+=maxpq.top();
-}
+
+            if(previous!=-1){
+               dp[i]=v[i][2]+dp[previous]+v[i][1]-v[i][0]; 
+            }else{
+                 dp[i]=v[i][2]+v[i][1]-v[i][0];
+                }
+        
+            dp[i]=max(dp[i],dp[i-1]);
             
-   pq.push({v[t][1],profit});
-
-
-t++;
-}
-
-
-}
-           while(!pq.empty() &&pq.top().first<=n){
-                maxpq.push(pq.top().second);
-                pq.pop();
-            }
-        return maxpq.top();
+        }
+    
         
-        
+        return dp[n-1];
         
     }
 };
