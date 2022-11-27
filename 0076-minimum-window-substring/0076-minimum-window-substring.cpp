6016227da76809f3c570533b9bccab6769f280d4
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-11-27 09:51:51 +0530

    Time: 88 ms (16.76%), Space: 8 MB (37.93%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-     unordered_map<char,int> st;
-        for(auto a:t){
-            st[a]+=1;
+        unordered_map<char,int> mp;
+        for( auto it :t){
+            mp[it]+=1;
         }
-               string ss;
-        int low = 0 ;
+        int ans = INT_MAX;
+        
+        
+        int low = 0;
         int high = 0;
         int size = s.size();
-   int ans = INT_MAX;
-        int temp = 0;
-        int n = st.size();
+        int cnt = 0;
+   int start = 0;
         while(high<size){
-            if(st.find(s[high])!=st.end()){
-                st[s[high]]-=1;
-                if(st[s[high]]==0){
-                    temp++;
+            
+            if(mp.count(s[high])){
+                mp[s[high]]--;
+                if(mp[s[high]]==0){
+                    cnt++;
                 }
             }
-           
-            while(low<high &&st.find(s[low])==st.end() ||st.find(s[low])!=st.end()
-                  &&st[s[low]]<0){
-     
-                if(st.find(s[low])!=st.end()
-                  &&st[s[low]]<0){
-                st[s[low]]+=1;
+            
+            while(low<=high && !mp.count(s[low]) || low<=high && mp.count(s[low]) &&mp[s[low]]<0){
+                if(mp.count(s[low])){
+                    mp[s[low]]+=1;
                 }
                 low++;
             }
-            
-            if(temp==n){
-
-                if(high-low+1<ans){
-                ss=s.substr(low,high-low+1);
+            if(cnt==mp.size()){
+                if(high-low+1  < ans){
+                   start = low ;
                     ans = high-low+1;
                 }
+               
             }
+            high++;
             
-         high++;   
+            
         }
- 
-        return ss;
-        
-        
-        
+        if(ans==INT_MAX) return "";
+        return s.substr(start,ans);
     }
 };
