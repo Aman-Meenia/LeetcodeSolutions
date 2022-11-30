2167cfac83074cf26b61dab394a9bfad196aed1e
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-11-30 07:07:31 +0530

    Time: 99 ms (25.77%), Space: 13.6 MB (20.11%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& v) {
+        queue<pair<int,pair<int,int> >> q;
+        q.push(make_pair(0,make_pair(0,v[0][0])));
+        int ans = INT_MAX;
+        int n = v.size();
+        int m = v[0].size();
+        int array1[4]={0,0,1,-1};
+        int array2[4]={1,-1,0,0};
+        vector<vector<int> >dp(n,vector<int>(m,INT_MAX));
+        dp[0][0]=v[0][0];
+        while(!q.empty()){
+int a = q.front().first;
+            int b = q.front().second.first;
+            int val = q.front().second.second;
+            if(a==n-1 && b==m-1){
+                ans=min(ans,val);
+            }
+            q.pop();
+            
+            for(int k=0; k<4; k++){
+                int x = a+array1[k];
+                int y = b+array2[k];
+                  
+    if(x>=0 && y>=0 && x<n && y<m && dp[x][y]>max(v[x][y],val)){
+
+        dp[x][y]=max(v[x][y],val);
+        
+        if(x==n-1 && y == m-1){
+            ans = min(ans,dp[x][y]);
+            // cout<<"Ans "<<ans<<endl;
+            // break;
+        }
+        
+        q.push(make_pair(x,make_pair(y,dp[x][y])));
+    }
+                
+                
+            }
+            
+            
+            
+        }
+        // for(auto a:dp){
+        //     for(auto b:a){
+        //         cout<<b<<" ";
+        //     }cout<<endl;
+        // }
+        
+        
+        
+        return ans;
+    }
+};
