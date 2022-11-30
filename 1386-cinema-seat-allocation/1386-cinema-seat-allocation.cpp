3f06a909086dcc3879c6f86cb5ae32fbc8d1cf68
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-11-30 20:21:47 +0530

    Time: 467 ms (36.17%), Space: 82.4 MB (15.24%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,49 +1,32 @@
 class Solution {
 public:
-        unordered_map<int,set<int> > mp;
-    
-    bool fun(int start ,int end, int k){
-        
-        
-        for(int i=start; i<=end; i++){
-            if(mp[k].count(i)){
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    
-    
-    
-    int maxNumberOfFamilies(int n, vector<vector<int>>& v) {
-        
-    
-        for(auto it:v){
+    int maxNumberOfFamilies(int n, vector<vector<int>>& temp) {
+        unordered_map<int,vector<int> > mp;
+        for(auto it : temp){
             int a = it[0];
             int b = it[1];
-            mp[a].insert(b);
+            mp[a].push_back(b);
         }
-        int ans =0;
-        int size =mp.size();
-        ans = (n-size)*2;
+        int ans = 0;
+        ans =(n- mp.size())*2;
+        for(auto it : mp){
+            vector<int> v(11,0);
+            
+            for(auto itt :it.second){
+                v[itt]=1;
+            }
         
-        for(auto it :mp){
-          if(fun(2,9,it.first)){
-              ans+=2;
-          }  else if(fun(4,7,it.first)){
-              ans+=1;
-          }else if(fun(2,5,it.first)){
-              ans+=1;
-          }else if(fun(6,9,it.first)){
-              ans+=1;
-          }
-            
-            
+            if(v[2]!=1 && v[3]!=1 && v[4]!=1 && v[5]!=1 &&v[6]!=1 && v[7]!=1 && v[8]!=1 && v[9]!=1){
+                ans+=2;
+            }else if(v[2]!=1 && v[3]!=1 && v[4]!=1 && v[5]!=1){
+                ans+=1;
+            }else if(v[6]!=1 && v[7]!=1 && v[8]!=1 && v[9]!=1){
+                ans+=1;
+            }else if( v[4]!=1 && v[5]!=1 &&v[6]!=1 && v[7]!=1){
+                ans+=1;
+            }
         }
-        
         return ans;
-        
         
     }
 };
