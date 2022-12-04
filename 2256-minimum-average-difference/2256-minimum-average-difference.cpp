51e672421059296ffbf0e2b5c9026a6e2e8e05d1
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-12-04 09:24:59 +0530

    Time: 141 ms (84.47%), Space: 84.7 MB (28.51%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,12 +1,9 @@
 #define ll long long
 class Solution {
 public:
-    int minimumAverageDifference(vector<int>& v) {        int n = v.size();
-        
+    int minimumAverageDifference(vector<int>& v) {
+        int n = v.size();
         vector<ll> left(n);
-        vector<ll> right(n);
-        // int n = v.size();
-        
         for(int i=0; i<n; i++){
             if(i==0){
                 left[i]=v[i];
@@ -15,36 +12,32 @@ public:
             left[i]=left[i-1]+v[i];
         }
         
-        
-        for(int i=n-1; i>=0;i--){
-            if(i==n-1){
-                right[i]=v[i];
-                continue;
-            }
-            right[i]=right[i+1]+v[i];
-        }
         int ans = INT_MAX;
+
         int index = 0;
         for(int i=0; i<n; i++){
+            
             if(i==n-1){
-ll temp = left[i]/n;
-                if(temp<ans){
-                    ans = temp;
-                    index=i;
-                }
-
+                ll temp = left[i]/n;
+                 if(temp<ans){
+                ans  = temp;
+                index = i;
+            }
                 break;
             }
-            ll ans1 = left[i]/(i+1);
-            ll ans2= right[i+1]/(n-(i+1));
-
+            ll ans1=left[i]/(i+1);
+            ll ans2=left[n-1]-left[i];
+            ans2=ans2/(n-i-1);
             ll temp = abs(ans1-ans2);
             if(temp<ans){
-                    ans = temp;
-                    index=i;
-                }
-  
+                ans  = temp;
+                index = i;
+            }
+            
         }
+        
+        
         return index;
+        
     }
 };
