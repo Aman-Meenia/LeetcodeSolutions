0117626d62e2711c258e576fe6a307194b91bf5b
Aman Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2023-09-28 13:01:57 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -7,34 +7,52 @@ using namespace std;
 class Solution
 {
     public:
-    int dp[1001][1001];
+//     int dp[1001][1001];
 
-int Knapsack(int v[] , int wt[] , int i , int weight){
+// int Knapsack(int v[] , int wt[] , int i , int weight){
 
-if(weight==0 || i<0){
-    return 0;
-}
-if(dp[i][weight]!=-1) return dp[i][weight];
+// if(weight==0 || i<0){
+//     return 0;
+// }
+// if(dp[i][weight]!=-1) return dp[i][weight];
 
-int ans1 =0 , ans2 =0;
-if(wt[i]<=weight){
-   ans1= v[i]+Knapsack(v,wt,i-1,weight-wt[i]);
-}
-ans2 = Knapsack(v,wt,i-1,weight);
-
-
-return dp[i][weight]=max(ans1,ans2);
+// int ans1 =0 , ans2 =0;
+// if(wt[i]<=weight){
+//   ans1= v[i]+Knapsack(v,wt,i-1,weight-wt[i]);
+// }
+// ans2 = Knapsack(v,wt,i-1,weight);
 
 
+// return dp[i][weight]=max(ans1,ans2);
 
-}
+
+
+// }
 
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int weight, int wt[], int v[], int n) 
     { 
         
-        memset(dp,-1,sizeof dp);
-        return Knapsack(v,wt,n-1,weight);
+        // memset(dp,-1,sizeof dp);
+        int dp[n+1][weight+1];
+memset(dp,0,sizeof dp);
+
+for(int i=1; i<=n; i++){
+    for(int j=1; j<=weight; j++){
+        int ans1 = 0, ans2=0;;
+if(wt[i-1]<=j){
+   ans1= v[i-1]+dp[i-1][j-wt[i-1]];
+}
+ans2 = dp[i-1][j];
+
+
+dp[i][j]=max(ans1,ans2);
+    }
+}
+
+
+
+        return dp[n][weight];
        // Your code here
     }
 };
